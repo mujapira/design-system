370d0f162048f6982b2430cf371c1727fae1f5a8
Mujapira <malmasmello2@gmail.com>
2023-02-05 17:13:41 -0300

    Improvements in Box, Text and HEading

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -8,6 +8,28 @@ export default {
     children:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti cumque, quisquam consequuntur perspiciatis esse fugit autem, voluptates repellat provident quod fugiat non illo nulla impedit id labore expedita natus! Eligendi.',
   },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
