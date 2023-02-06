490fe1306f018a0e180369ca6140ec234a17902f
Mujapira <malmasmello2@gmail.com>
2023-02-06 10:35:01 -0300

    text input fix 2

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -1,5 +1,5 @@
-import { ComponentProps, ElementRef, forwardRef } from 'react'
-import { Input, Prefix, TextInputContainer } from './styles'
+import { ComponentProps, ElementRef, forwardRef } from "react";
+import { Input, Prefix, TextInputContainer } from "./styles";
 
 export interface TextInputProps
   extends Omit<ComponentProps<typeof Input>, 'size'> {
@@ -14,8 +14,8 @@ export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
         {!!prefix && <Prefix>{prefix}</Prefix>}
         <Input ref={ref} {...props} />
       </TextInputContainer>
-    )
-  },
-)
+    );
+  }
+);
 
-TextInput.displayName = 'TextInput'
+TextInput.displayName = "TextInput";
